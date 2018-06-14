80ad0c965be9db9b50371df1a84d512394b72998
瞿龙俊 <qulongjun@qulongjun.local>
2018-06-14 09:05:35 +0800

    封装工具类

---
+++ src/plugins/utils/_utils.js
@@ -0,0 +1,24 @@
+/**
+ * Created by qulongjun on 2018/6/10.
+ */
+/**
+ * @class oUtils:Helper Functions
+ *
+ */
+
+const oUtils = {
+    /**
+     * 计算最终展示的 Class 修饰符
+     * @param _includeClass 用户自定义需包含的 Class
+     * @param _excludeClass 用户自定义需移除的 Class
+     * @param _uniqueClass 用户自定义唯一性 Class
+     * @param _sysClass 系统自带的 Class
+     * @private
+     */
+    _computedClass(_includeClass, _excludeClass, _uniqueClass, ..._sysClass) {
+        if (_uniqueClass && Array.isArray(_uniqueClass)) {
+
+        }
+
+    }
+};
