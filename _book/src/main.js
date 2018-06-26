52622da3504412502086fec1ffdd412f5dd62815
瞿龙俊 <154256698@qq.com>
2018-06-26 14:59:39 +0800

    remove _book

--- _book/src/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import RouteConfig from './routes/index';
-
-
-ReactDOM.render(RouteConfig, document.getElementById('root'));
--- _book/src/plugins/config.js
+++
@@ -1,2 +0,0 @@
-import '../plugins/jquery'
-import 'bootstrap'
